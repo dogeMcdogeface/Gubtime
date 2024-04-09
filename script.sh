29c9f5914aba5e890cc7bb1835b87f9d3e6fe9ff
dogeMcdogeface <79205861+dogeMcdogeface@users.noreply.github.com>
2024-04-09 20:44:28 +0200

    ss

--- script.sh
+++ script.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
 # Define the YAML string
-yaml_string="urls:
-  - Entry: 
-    url: https://example.com
-    alias: Example
-    description: Example website
-  - Entry: 
-    url: https://google.com
-    alias: Example
-    description: Probably always up, but gonna return 30x (moved)
-  - Entry: 
-    url: https://nonexistentwebsite.noway
-    alias: Nonexistent Example
-    description: Another example website"
+yaml_string="$1"
 
 echo "--------------------------------------------------------------------------"
 echo "$1"
