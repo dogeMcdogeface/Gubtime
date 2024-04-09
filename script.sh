7a71645417d1f42c04f0ff5354bf416c778c7486
dogeMcdogeface <79205861+dogeMcdogeface@users.noreply.github.com>
2024-04-09 13:00:43 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,5 +3,8 @@
 # Print the string passed as input
 echo "Input string: $1"
 
+# Print the secret variable
+echo "Secret variable content: $2"
+
 # Curl Google and print the return code
 curl -I -s http://www.google.com -o /dev/null -w "%{http_code}\n"
