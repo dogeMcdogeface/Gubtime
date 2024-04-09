52c1ec886f4b97ef2e47ea3989c42dab04eea31d
dogeMcdogeface <79205861+dogeMcdogeface@users.noreply.github.com>
2024-04-09 16:28:59 +0200

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use reqwest::blocking::Client;
 use serde::{Deserialize, Serialize};
 use std::env;
@@ -15,47 +16,94 @@ struct UrlEntry {
     description: String,
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+struct ResponseData {
+    timestamp: DateTime<Utc>,
+    responses: Vec<ResponseEntry>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct ResponseEntry {
+    alias: String,
+    description: String,
+    response: Option<u16>,
+}
+
 fn main() {
     // Retrieve command line arguments
     let args: Vec<String> = env::args().collect();
 
+    // Check if the program is run with the -v option
+    let verbose_mode = true; // args.iter().any(|arg| arg == "-v");
+
     // Check if there is an argument passed
-    let input_string = if args.len() > 1 {
-        args[1].clone() // If argument is present, use it
+    let input_string = if args.len() > 1 && !verbose_mode {
+        args[1].clone() // If argument is present and not in verbose mode, use it
     } else {
-        // If no argument provided, use the default value
+        // If no argument provided or in verbose mode, use the default value
         String::from(
-            "urls:\n  - url: https://example.com\n    alias: Example\n    description: Example website\n  - url: https://example.org\n    alias: AnotherExample\n    description: Another example website",
+            "urls:\n  - url: https://example.com\n    alias: Example\n    description: Example website\n  - url: https://nonexistentwebsite.noway\n    alias: AnotherExample\n    description: Another example website",
         )
     };
 
-    // Print greeting message
-    println!("Hello!");
+    // Print greeting message if in verbose mode
+    if verbose_mode {
+        println!("Hello!");
+    }
 
-    // Print the value of the string
-    println!("{}", input_string);
+    let mut my_vector: Vec<ResponseEntry> = Vec::new();
 
     // Parse the YAML string
     match serde_yaml::from_str::<UrlData>(&input_string) {
         Ok(data) => {
-            // Print the list of URLs
-            println!("List of URLs:");
+            // Print the list of URLs if in verbose mode
+            if verbose_mode {
+                println!("List of URLs:");
+            }
             for url_entry in data.urls {
-                println!("Alias: {}", url_entry.alias);
-                println!("Description: {}", url_entry.description);
-                println!("URL: {}", url_entry.url);
-
+                // Print alias, description and URL if in verbose mode
+                if verbose_mode {
+                    println!("Alias: {}", url_entry.alias);
+                    println!("Description: {}", url_entry.description);
+                    println!("URL: {}", url_entry.url);
+                }
+                let mut a = ResponseEntry {
+                    alias: url_entry.alias,
+                    description: url_entry.description,
+                    response: None,
+                };
                 // Make HTTP request to the URL
                 let client = Client::new();
                 match client.get(&url_entry.url).send() {
                     Ok(response) => {
-                        println!("HTTP Response Code: {}", response.status());
+                        // Print HTTP response code if in verbose mode
+                        if verbose_mode {
+                            println!("HTTP Response Code: {:#?}", response.status());
+                            a.response = Some(response.status().as_u16());
+                        }
                     }
                     Err(e) => {
-                        eprintln!("Error making HTTP request: {:?}{:?}", e,  e.status());
+                        // Print error if in verbose mode
+                        if !e.status().is_none() {
+                            if verbose_mode {
+                                println!("HTTP Response Code: {:#?}", e.status());
+                            }
+                        } else {
+                            eprintln!("Error making HTTP request: {:#?}", e);
+                        }
                     }
                 }
+                my_vector.push(a);
             }
+            // Convert the vector to YAML
+            let response = ResponseData {
+                timestamp: Utc::now(),
+                responses: my_vector,
+            };
+            let yaml_string = serde_yaml::to_string(&response).unwrap();
+
+            // Print the YAML string
+            println!("{}", yaml_string);
         }
         Err(e) => {
             eprintln!("Error parsing YAML: {}", e);
