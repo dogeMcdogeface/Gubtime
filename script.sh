4f579d355e6dec623800de4b830398f3e09cd99d
dogeMcdogeface <79205861+dogeMcdogeface@users.noreply.github.com>
2024-04-09 13:11:14 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,16 +2,20 @@
 
 # Parse YAML content from the secret variable
 urls_yaml="$1"
+aliases=$(echo "$urls_yaml" | yq eval '.urls[].alias' -)
+descriptions=$(echo "$urls_yaml" | yq eval '.urls[].description' -)
+urls=$(echo "$urls_yaml" | yq eval '.urls[].url' -)
 
 # Print the string passed as input
 echo "Input string: $2"
 
 # Loop through each URL, curl, and print the status code
-while IFS= read -r line; do
-    alias=$(echo "$line" | yq eval '.alias' -)
-    url=$(echo "$line" | yq eval '.url' -)
-    
-    echo "Curling URL: $alias"
+index=0
+for url in $urls; do
+    alias=$(echo "$aliases" | sed -n "${index + 1}p")
+    description=$(echo "$descriptions" | sed -n "${index + 1}p")
+    echo "Curling URL Alias: $alias - Description: $description"
     status_code=$(curl -I -s "$url" -o /dev/null -w "%{http_code}")
     echo "Status code for $alias: $status_code"
-done <<< $(echo "$urls_yaml" | yq eval '.urls[]' -)
+    ((index++))
+done
