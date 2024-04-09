0fbd7d13b609e7b11fd3297efdeaae2aa33ebc06
dogeMcdogeface <79205861+dogeMcdogeface@users.noreply.github.com>
2024-04-09 17:43:59 +0200

    update

--- script.sh
+++ script.sh
@@ -1,27 +1,5 @@
-# Define the YAML string
-yaml_string="urls:
-  - url: https://example.com
-    alias: Example
-    description: Example website
-  - url: https://nonexistentwebsite.noway
-    alias: Another Example
-    description: Another example website"
-
-# Print timestamp
-timestamp=$(date -u +"%Y-%m-%dT%H:%M:%S.%NZ")
-echo "timestamp: $timestamp"
-
-# Parse YAML string and check responses
-while IFS= read -r line; do
-    if [[ "$line" =~ ^\ +url:\ (.*) ]]; then
-        url="${BASH_REMATCH[1]}"
-    elif [[ "$line" =~ ^\ +alias:\ (.*) ]]; then
-        alias="${BASH_REMATCH[1]}"
-    elif [[ "$line" =~ ^\ +description:\ (.*) ]]; then
-        description="${BASH_REMATCH[1]}"
-        http_status=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-        echo "- alias: $alias"
-        echo "  description: $description"
-        echo "  response: $http_status"
+  if ! curl -s www.google.com -o /dev/null; then
+    echo "Failed to reach target "
+    else
+    echo "sssss"
     fi
-done <<< "$yaml_string"
