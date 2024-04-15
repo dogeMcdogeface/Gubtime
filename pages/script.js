9bbd343b919f2525630538dc830d26e757b863a5
dogeMcdogeface <79205861+dogeMcdogeface@users.noreply.github.com>
2024-04-16 00:46:38 +0200

    Updated web pages to work with log fetching script

---
+++ pages/script.js
@@ -0,0 +1,62 @@
+const latestUrl = "https://dogemcdogeface.github.io/Gubtime/logs/latest.txt";
+let logsData;
+let maxLogFiles = 8;	//TODO
+
+// Async function to fetch data from URL
+async function fetchData(url) {
+    try {
+        console.log("Loading file: ", url);
+        const response = await fetch(url);
+        const data = await response.text();
+        return data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return null;
+    }
+}
+
+// Send signal after loading data
+async function loadDataAndSendSignal() {
+    const latestData = await fetchData(latestUrl);
+    console.log("Latest: ", latestData);
+
+    if (latestData) {
+        // Parse latest.txt to get log information
+        const logInfo = jsyaml.load(latestData);
+        const logRepo = logInfo.LOG_REPO;
+        const logBrnc = logInfo.LOG_BRNC;
+        const logPath = logInfo.LOG_PATH;
+        const logName = logInfo.LOG_NAME;
+        let logWeek = parseInt(logInfo.LOG_WEEK); // Convert to number for calculation
+        const logType = logInfo.LOG_TYPE;
+
+        while (logWeek >= 0) {
+            const logFile = `${logRepo}/${logBrnc}/${logPath}${logName}${logWeek}${logType}`;
+            console.log("Composed Log Path: ", logFile);
+            // Construct URL for the latest log file
+            const logUrl = `https://raw.githubusercontent.com/${logFile}	`;
+            // Fetch the log file
+            const yamlData = await fetchData(logUrl);
+            logWeek--;
+            if (yamlData) {
+                const tmpData = jsyaml.load(yamlData);
+                if (!logsData || logsData.length === 0) {
+                    logsData = tmpData;
+                } else {
+                    logsData = tmpData.concat(logsData);
+                }
+                console.log(logsData);
+                // Dispatch a custom event to signal that data is loaded
+                document.dispatchEvent(new CustomEvent("dataLoaded", { detail: logsData }));
+                document.dispatchEvent(new CustomEvent("newdataLoaded", { detail: tmpData }));
+            } else {
+                console.error("Failed to fetch log data.");
+            }
+        }
+    } else {
+        console.error("Failed to fetch latest log information.");
+    }
+}
+
+// Call the function to load data and send signal
+loadDataAndSendSignal();
